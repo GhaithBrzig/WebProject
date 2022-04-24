c587f53c8dfa0c479757ea9f034ce18dbf4f7d12
iheblafi <iheb.lafi@esprit.tn>
2022-04-24 23:03:16 +0100

    FirstCommit

---
+++ src/Entity/User.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="`user`")
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    public $confirm_password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Fname;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $Bdate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getFname(): ?string
+    {
+        return $this->Fname;
+    }
+
+    public function setFname(string $Fname): self
+    {
+        $this->Fname = $Fname;
+
+        return $this;
+    }
+
+    public function getBdate(): ?\DateTimeInterface
+    {
+        return $this->Bdate;
+    }
+
+    public function setBdate(\DateTimeInterface $Bdate): self
+    {
+        $this->Bdate = $Bdate;
+
+        return $this;
+    }
+}
